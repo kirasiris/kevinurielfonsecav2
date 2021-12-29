03e62ce31ed0bfdc365004068afe40a81d987a2e
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2021-12-28 18:07:20 -0600

    Adding featured image on rest api

--- functions.php
+++ functions.php
@@ -52,6 +52,31 @@ add_action('after_setup_theme','wpb_theme_setup');
 
 /*
 *
+* FEATURED IMAGE ON REST API
+*
+*/
+
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
+
+/*
+*
 * NO FEATURED IMAGE?
 *
 */
